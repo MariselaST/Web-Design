240ca663437d6b9fdee1415d169d36979678d305
MariselaST <40775075+MariselaST@users.noreply.github.com>
2018-09-19 09:27:45 -0500

    added condition

--- W3/JavaScript/condiciones/condiciones.js
+++ W3/JavaScript/condiciones/condiciones.js
@@ -1,11 +1,15 @@
 var age = prompt ("Cuantos años tienes?");
 
 if(age<0){
-	alert("Edad negativa");
+	alert("Error: Edad negativa");
 }
 
 else if(age==21){
 	alert("Feliz cumpleaños 21");
+}
+
+else if (!(age%2===0)){
+	alert("Tu edad es impar")
 }
 
 else if (age % Math.sqrt(age)===0){
